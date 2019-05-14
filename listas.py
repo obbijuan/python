f2582bc1478335e54ddf1b64f66fe7b08d5a8318
obbijuan <juan.garces@outlook.com>
2019-05-13 23:50:34 -0400

    agregando mas ejemplos

--- listas.py
+++ listas.py
@@ -14,6 +14,27 @@ print '------------------------------'
 a = ['a','b','c']
 b = [1,2,3]
 x = [a,b]
-print x         # Imprime las listas
-print x[1]      # Imprime la segunda lista
-print x[0][2]   # Imprime el tercer elemento de la primera lista
+print x                 # Imprime las listas
+print x[1]              # Imprime la segunda lista
+print x[0][2]           # Imprime el tercer elemento de la primera lista
+print '------------------------------'
+s = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
+s[2:5] = []     # Elimina un segmento de la lista
+print s
+s[:] = []       # Elimina todos los elementos
+print s
+print '------------------------------'
+n = [1,2,3,4,5,6,7,8,9]
+n.append(100)       # Agrega el elemento al final de la lista
+print n
+n.insert(0,200)     # Agrega un elemento al inicio de la lista
+print n
+n.pop()             # Elimina el ultimo elemento
+print n
+n.pop(0)            # Elimina el primer elemento de la lista
+print n
+del n[2]            # Elinina el elemento de la posicion indicada
+print n
+r = [1,2,3,3,3,4,5]
+r.remove(3)
+print r             # Eliina el primer numero repetido
