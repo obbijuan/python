53111a10840b383e364dd9c8354d67cf1c447187
obbijuan <juan.garces@outlook.com>
2019-05-13 23:06:02 -0400

    agregado

---
+++ listas.py
@@ -0,0 +1,19 @@
+# Listas
+
+lista = [1,2,3,4,5,6,7,8,9]
+print lista
+print lista[3]
+print lista[-1]     # Imprime el ultimo dato
+print lista[-2]     # Imprime el penultimo dato
+print lista[2:5]    # Imprime un segmento de la lista
+print len(lista)    # Imprime la longitud
+print list('asdfghjw')
+print lista[::2]    # Devuelve numeros impares
+print lista[::-1]   # Datos de forma inversa
+print '------------------------------'
+a = ['a','b','c']
+b = [1,2,3]
+x = [a,b]
+print x         # Imprime las listas
+print x[1]      # Imprime la segunda lista
+print x[0][2]   # Imprime el tercer elemento de la primera lista
