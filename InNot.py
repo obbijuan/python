dba8a32764f5daf790b50506fb34751bf0e0d34c
obbiejuan <juan.garces@outlook.com>
2019-05-16 14:29:02 -0400

    Uso de In y Not

---
+++ InNot.py
@@ -0,0 +1,14 @@
+y = [1,2,3]
+x = 1
+
+if x in y:
+    print ('In')
+
+if x not in y:
+    print('Not In')
+
+is_ok = True
+
+# Se recomienda usar not con tipos booleandos
+if not is_ok:
+    print('Hello')
