fa9e917bd8cc85a6af302afc85d7edb19e176458
obbijuan <juan.garces@outlook.com>
2019-05-22 23:18:30 -0400

    Uso de range

---
+++ Flujos/range.py
@@ -0,0 +1,11 @@
+
+num_list = [0,1,2,3,4,5,6,7,8,9]
+
+# Comienza desde el 2 hasta el 10
+for i in range(2,10):
+    print (i)
+print ('-------------------')
+
+# Comienza desde el 2 y va de 3 en 3
+for i in range(2,10,3):
+    print (i)
