d2534ee6f37986ffe8534a5ebcf1b61929995934
obbijuan <juan.garces@outlook.com>
2019-05-25 13:45:58 -0400

    Uso de funciones

---
+++ Funciones/funcion.py
@@ -0,0 +1,10 @@
+# Las funciones se definen antes de ser ejecutadas
+
+def say_something():
+    print('Hi!')
+
+def what_is_this(color):
+    print (color)
+
+say_something()
+what_is_this('blue!')
