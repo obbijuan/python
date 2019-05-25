c9dad228cd3ee6df1f39991b1481fdc6d5e44ab2
obbijuan <juan.garces@outlook.com>
2019-05-24 23:19:49 -0400

    Uso de for

---
+++ Flujos/for.py
@@ -0,0 +1,14 @@
+some_list = [1,2,3,4,5]
+
+for i in some_list:
+    print (i)
+
+for i in 'abc':
+    print (i)
+
+for word in ['My', 'name', 'is', 'Mike']:
+    if word=='Mike':
+        continue
+    print (word)
+else:
+    print('finaliza!')
