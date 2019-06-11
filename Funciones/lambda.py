3a8ada8e804ade51fb5f8eb0bfba0ad3488c0542
obbiejuan <juan.garces@outlook.com>
2019-06-11 14:01:09 -0400

    Uso de lambda

---
+++ Funciones/lambda.py
@@ -0,0 +1,11 @@
+l = ['Mon', 'tue', 'Wed', 'Thu', 'fri', 'sat', 'Sun']
+
+def change_words(words, func):
+	for word in words:
+		print(func(word))
+
+def sample_func(word):
+	return word.capitalize()
+
+
+change_words(l, sample_func)
