91bc78191cf75e42d3928f595443dec881cc55fc
obbiejuan <juan.garces@outlook.com>
2019-06-11 15:07:30 -0400

    Corrigiendo

--- Funciones/lambda.py
+++ Funciones/lambda.py
@@ -4,7 +4,6 @@ def change_words(words, func):
 	for word in words:
 		print(func(word))
 
-<<<<<<< HEAD
 # def sample_func(word):
 # 	return word.capitalize()
 
@@ -15,12 +14,3 @@ def change_words(words, func):
 # Con lambda se reduce la cantidad de codigo a escribir
 change_words(l, lambda word: word.capitalize())
 change_words(l, lambda word: word.lower())
-=======
-def sample_func(word):
-	return word.capitalize()
-
-
-change_words(l, sample_func)
-
-	# asdfasdfadfa
->>>>>>> 08e9e44b74cc18c4b077d4abc4f4384c8625006c
