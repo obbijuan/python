6ab992ff3a0f67abb39409c85b458d403f2f9141
obbijuan <juan.garces@outlook.com>
2019-06-23 00:00:40 -0400

    Uso de import para importar paquetes

---
+++ Modulos/importar.py
@@ -0,0 +1,8 @@
+# Al encontrar el archivo __init__.py, python reconocerá como paquete y
+# podrá importar el archivo utils.py. El archivo __init__.py es escencial
+# para crear un paquete
+
+import modulos.utils
+
+r = modulos.utils.say_twice('hellow')
+print(r)
